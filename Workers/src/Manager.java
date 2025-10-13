88db29b11881846426a75bf79125be2f886c5cd4
AaronFrancis05 <aarontaremwa8@gmail.com>
2025-10-13 10:03:49 +0300

    Commit 2
    In the New Version of Manager class from line 8 to line 17, a public method named ShowDetails that overrides the parent method in Employee class was added. In addition, a new  method named ConductMeeting was also added.

--- Workers/src/Manager.java
+++ Workers/src/Manager.java
@@ -5,4 +5,14 @@ public class Manager extends Employee {
         super(name, age, employeeId, salary);
         Department = department;
     }
+
+    @Override
+    public void ShowDetails() {
+        super.ShowDetails();
+        System.out.println("Department: " + Department);
+    }
+
+    public void ConductMeetings() {
+        System.out.println(Name + " is conducting a meeting in the " + Department + " department");
+    }
 }
