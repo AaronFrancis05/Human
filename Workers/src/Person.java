f04d52b13c90e10c735d92b33396fe43d9b074cf
AaronFrancis05 <aarontaremwa8@gmail.com>
2025-10-13 09:37:21 +0300

    Commit 2
    In the New Version of Person class from line 2 to line 17, public instance variables called Name, Age , a public constructor called Person and public methods called ShowDetails and Work were added.

--- Workers/src/Person.java
+++ Workers/src/Person.java
@@ -1,2 +1,18 @@
 public class Person {
+    public String Name;
+    public int Age;
+
+    public Person(String name, int age) {
+        Name = name;
+        Age = age;
+    }
+
+    public void ShowDetails() {
+        System.out.println("Name " + Name);
+        System.out.println("Age " + Age);
+    }
+
+    public void Work() {
+        System.out.println(Name + " " + "is doing some work");
+    }
 }
